6f005b5fb0217eb8738069664ab088f6008b987f
Soares Chen <soares.chen@maybevoid.com>
2025-08-21 08:29:19 +0000

    Add Provider param to SerializeFrom

--- crates/cgp-serde/src/providers/from.rs
+++ crates/cgp-serde/src/providers/from.rs
@@ -3,36 +3,35 @@ use cgp::prelude::*;
 use crate::components::{
     DeserializeImpl, DeserializeImplComponent, SerializeImpl, SerializeImplComponent,
 };
-use crate::providers::UseSerde;
 
-pub struct SerializeFrom<Target>(pub PhantomData<Target>);
+pub struct SerializeFrom<Target, Provider = UseContext>(pub PhantomData<(Target, Provider)>);
 
 #[cgp_provider]
-impl<Value, Target> SerializeImpl<Value> for SerializeFrom<Target>
+impl<Value, Target, Provider> SerializeImpl<Value> for SerializeFrom<Target, Provider>
 where
     Value: Clone + Into<Target>,
-    UseSerde: SerializeImpl<Target>,
+    Provider: SerializeImpl<Target>,
 {
     fn serialize<S>(value: &Value, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer,
     {
         let target = value.clone().into();
-        UseSerde::serialize(&target, serializer)
+        Provider::serialize(&target, serializer)
     }
 }
 
 #[cgp_provider]
-impl<'a, Value, Target> DeserializeImpl<'a, Value> for SerializeFrom<Target>
+impl<'a, Value, Target, Provider> DeserializeImpl<'a, Value> for SerializeFrom<Target, Provider>
 where
-    UseSerde: DeserializeImpl<'a, Target>,
+    Provider: DeserializeImpl<'a, Target>,
     Target: Into<Value>,
 {
     fn deserialize<D>(deserializer: D) -> Result<Value, D::Error>
     where
         D: serde::Deserializer<'a>,
     {
-        let target = UseSerde::deserialize(deserializer)?;
+        let target = Provider::deserialize(deserializer)?;
         Ok(target.into())
     }
 }
